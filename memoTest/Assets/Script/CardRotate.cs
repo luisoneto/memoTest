3fbcb6c4ca3471894c9e529831755a5ae382c6a9
luisoneto <luisoneto@gmail.com>
2021-11-10 13:54:10 -0300

    Show and Hide cards

--- memoTest/Assets/Script/CardRotate.cs
+++ memoTest/Assets/Script/CardRotate.cs
@@ -8,11 +8,14 @@ public class CardRotate : MonoBehaviour
     public float speed = 1.0f;
     public Quaternion rotation1 = Quaternion.Euler(0, 0, 0);
     public Quaternion rotation2 = Quaternion.Euler(0, 0, 180);
+    public Quaternion rotationShowCard = Quaternion.Euler(0, 0, 180);
+    public Quaternion rotationHideCard = Quaternion.Euler(0, 0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
+        StartCoroutine(ShowCards(rotationShowCard, rotationHideCard, 1f / speed));
     }
     void Update()
     {
@@ -41,5 +44,26 @@ public class CardRotate : MonoBehaviour
         }
         this.transform.rotation = finalRotation;
         isRotated = true;
+    }
+
+    IEnumerator ShowCards(Quaternion originalRotation, Quaternion finalRotation, float duration)
+    {
+        yield return new WaitForSeconds(5);
+
+        if (duration > 0f)
+        {
+            float startTime = Time.time;
+            float endTime = startTime + duration;
+            this.transform.rotation = originalRotation;
+            yield return null;
+            while (Time.time < endTime)
+            {
+                float progress = (Time.time - startTime) / duration;
+                // progress will equal 0 at startTime, 1 at endTime.
+                this.transform.rotation = Quaternion.Slerp(originalRotation, finalRotation, progress);
+                yield return null;
+            }
+        }
+        this.transform.rotation = finalRotation;
     }
 }
