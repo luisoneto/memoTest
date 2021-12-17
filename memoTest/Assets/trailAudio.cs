d8c4afaa5c55f9adc01abc4897ce6c88dbdf1dd0
luisoneto <luisoneto@gmail.com>
2021-12-17 13:54:00 -0300

    memoTest

---
+++ memoTest/Assets/trailAudio.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trailAudio : MonoBehaviour
+{
+    public AudioSource whistle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        whistle.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
