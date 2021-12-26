d1c753c0d590b58ef059439d188a1b46684fa67f
luisoneto <78429639+luisoneto@users.noreply.github.com>
2021-12-26 17:59:52 -0300

    memoryTest

--- memoTest/Assets/Script/CanvasController.cs
+++ memoTest/Assets/Script/CanvasController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasController : MonoBehaviour
 {
@@ -26,45 +27,45 @@ public class CanvasController : MonoBehaviour
         if (answer)
         {
             puntos = puntos + 3;
-            var pointText = GameObject.Find("text_points").GetComponent<TMP_Text>();
+            var pointText = GameObject.Find("GreenCoin").GetComponent<Image>();
             pointText.enabled = true;
             Vector3 OriginalPosition = pointText.transform.localPosition;
-            StartCoroutine(upText(pointText.transform.position, new Vector3(pointText.transform.position.x, 190, 0), 0.5f, pointText));
+            StartCoroutine(upText(pointText.transform.position, new Vector3(pointText.transform.position.x, 200, 0), 0.5f, pointText));
             StartCoroutine(DisappearOverTime(pointText.transform.localScale, 1.0f, pointText, OriginalPosition));
 
         }
         else
         {
             puntos = puntos - 1;
-            var pointText = GameObject.Find("text_lostpoint").GetComponent<TMP_Text>();
+            var pointText = GameObject.Find("RedCoin").GetComponent<Image>();
             pointText.enabled = true;
             Vector3 OriginalPosition = pointText.transform.localPosition;
-            StartCoroutine(upText(pointText.transform.position, new Vector3(pointText.transform.position.x, 190, 0), 0.5f, pointText));
+            StartCoroutine(upText(pointText.transform.position, new Vector3(pointText.transform.position.x, 200, 0), 0.5f, pointText));
             StartCoroutine(DisappearOverTime(pointText.transform.localScale, 1.0f, pointText, OriginalPosition));
         }
     }
 
 
-    IEnumerator upText(Vector3 originalPosition, Vector3 finalPosition, float duration, TMP_Text text)
+    IEnumerator upText(Vector3 originalPosition, Vector3 finalPosition, float duration, Image coin)
     {
         if (duration > 0f)
         {
 
             float startTime = Time.time;
             float endTime = startTime + duration;
-            text.transform.position = originalPosition;
+            coin.transform.position = originalPosition;
             yield return null;
             while (Time.time < endTime)
             {
                 float progress = (Time.time - startTime) / duration;
-                text.transform.position = Vector3.Lerp(text.transform.position, finalPosition, progress);
+                coin.transform.position = Vector3.Lerp(coin.transform.position, finalPosition, progress);
                 yield return null;
             }
         }
-        text.transform.position = finalPosition;
+        coin.transform.position = finalPosition;
     }
 
-    IEnumerator DisappearOverTime(Vector3 originalScale, float duration, TMP_Text text, Vector3 originalPosition)
+    IEnumerator DisappearOverTime(Vector3 originalScale, float duration, Image coin, Vector3 originalPosition)
     {
 
         yield return new WaitForSeconds(0.40f);
@@ -78,13 +79,13 @@ public class CanvasController : MonoBehaviour
             while (Time.time < endTime)
             {
                 float progress = (Time.time - startTime) / duration;
-                text.transform.localScale = Vector3.Slerp(text.transform.localScale, new Vector3(0, 0, 0), progress);
+                coin.transform.localScale = Vector3.Slerp(coin.transform.localScale, new Vector3(0, 0, 0), progress);
                 yield return null;
             }
         }
 
-        text.enabled = false;
-        text.transform.localPosition = originalPosition;
-        text.transform.localScale = originalScale;
+        coin.enabled = false;
+        coin.transform.localPosition = originalPosition;
+        coin.transform.localScale = originalScale;
     }
 }
